e0f44ae90bb2a54b3b4948931c3518542531e9f5
fluorspar20 <amoghdixit104@gmail.com>
2021-03-19 22:34:03 +0530

    basic editing implemented

--- tsr-frontend/src/components/Step2/Step2.js
+++ tsr-frontend/src/components/Step2/Step2.js
@@ -1,13 +1,135 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 
-import "../Tab/Tab.css";
+import Slider from './Slider/Slider'
+import SidebarItem from './Sidebar/Sidebar'
 
-export default class Step2 extends Component {
-    render() {
-        return (
-            <div id="stp2">
-                <p>This is step2</p>
-            </div>
-        )
+import './Step2.css';
+
+const DEFAULT_OPTIONS = [
+    {
+        name: 'Brightness',
+        property: 'brightness',
+        value: 100,
+        range: {
+            min: 0,
+            max: 200
+        },
+        unit: '%'
+    },
+    {
+        name: 'Contrast',
+        property: 'contrast',
+        value: 100,
+        range: {
+            min: 0,
+            max: 200
+        },
+        unit: '%'
+    },
+    {
+        name: 'Saturation',
+        property: 'saturate',
+        value: 100,
+        range: {
+            min: 0,
+            max: 200
+        },
+        unit: '%'
+    },
+    {
+        name: 'Grayscale',
+        property: 'grayscale',
+        value: 0,
+        range: {
+            min: 0,
+            max: 100
+        },
+        unit: '%'
+    },
+    {
+        name: 'Sepia',
+        property: 'sepia',
+        value: 0,
+        range: {
+            min: 0,
+            max: 100
+        },
+        unit: '%'
+    },
+    {
+        name: 'Hue',
+        property: 'hue-rotate',
+        value: 0,
+        range: {
+            min: 0,
+            max: 360
+        },
+        unit: 'deg'
+    },
+    {
+        name: 'Blur',
+        property: 'blur',
+        value: 0,
+        range: {
+            min: 0,
+            max: 20
+        },
+        unit: 'px'
     }
+]
+
+function Step2() {
+    const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
+    const [options, setOptions] = useState(DEFAULT_OPTIONS)
+    const selectedOption = options[selectedOptionIndex]
+
+    function handleSliderChange({ target }) {
+        setOptions(prevOptions => {
+            return prevOptions.map((option, index) => {
+                if (index !== selectedOptionIndex) return option
+                return { ...option, value: target.value }
+            })
+        })
+    }
+
+    function getImageStyle() {
+        const filters = options.map(option => {
+            return `${option.property}(${option.value}${option.unit})`
+        })
+
+        return { filter: filters.join(' ') }
+    }
+
+    console.log(getImageStyle())
+
+    return (
+        <div className="container2">
+            <div className="container1" id="stp2">
+                <div className="main-image" style={getImageStyle()}>
+                    <img src="/assets/traffic.jpg" alt="Image" height="300" width="auto" />
+                </div>
+                <div className="sidebar">
+                    {options.map((option, index) => {
+                        return (
+                            <SidebarItem
+                                key={index}
+                                name={option.name}
+                                active={index === selectedOptionIndex}
+                                handleClick={() => setSelectedOptionIndex(index)}
+                            />
+                        )
+                    })}
+                </div>
+            </div>
+            <Slider
+                min={selectedOption.range.min}
+                max={selectedOption.range.max}
+                value={selectedOption.value}
+                handleChange={handleSliderChange}
+            />
+        </div>
+
+    )
 }
+
+export default Step2;
