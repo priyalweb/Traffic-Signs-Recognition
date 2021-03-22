93952d79044c03e8b08e631759b0204cbc75c97e
fluorspar20 <amoghdixit104@gmail.com>
2021-03-22 12:53:54 +0530

    basic step2 done

--- tsr-frontend/src/components/Step2/Step2.js
+++ tsr-frontend/src/components/Step2/Step2.js
@@ -1,135 +1,82 @@
 import React, { useState } from 'react';
 
-import Slider from './Slider/Slider'
 import SidebarItem from './Sidebar/Sidebar'
-
-import Step1 from '../Step1/Step1';
 import './Step2.css';
 
-const DEFAULT_OPTIONS = [
-    {
-        name: 'Brightness',
-        property: 'brightness',
-        value: 100,
-        range: {
-            min: 0,
-            max: 200
-        },
-        unit: '%'
-    },
-    {
-        name: 'Contrast',
-        property: 'contrast',
-        value: 100,
-        range: {
-            min: 0,
-            max: 200
-        },
-        unit: '%'
-    },
-    {
-        name: 'Saturation',
-        property: 'saturate',
-        value: 100,
-        range: {
-            min: 0,
-            max: 200
-        },
-        unit: '%'
-    },
-    {
-        name: 'Grayscale',
-        property: 'grayscale',
-        value: 0,
-        range: {
-            min: 0,
-            max: 100
-        },
-        unit: '%'
-    },
-    {
-        name: 'Sepia',
-        property: 'sepia',
-        value: 0,
-        range: {
-            min: 0,
-            max: 100
-        },
-        unit: '%'
-    },
-    {
-        name: 'Hue',
-        property: 'hue-rotate',
-        value: 0,
-        range: {
-            min: 0,
-            max: 360
-        },
-        unit: 'deg'
-    },
-    {
-        name: 'Blur',
-        property: 'blur',
-        value: 0,
-        range: {
-            min: 0,
-            max: 20
-        },
-        unit: 'px'
-    }
-]
+import { DEFAULT_OPTIONS } from '../../utils/augs'
 
 const Step2 = (props) => {
     const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
-    const [options, setOptions] = useState(DEFAULT_OPTIONS)
-    const selectedOption = options[selectedOptionIndex]
 
-    function handleSliderChange({ target }) {
-        setOptions(prevOptions => {
-            return prevOptions.map((option, index) => {
-                if (index !== selectedOptionIndex) return option
-                return { ...option, value: target.value }
-            })
+    function handleSubmit(e) {
+        e.preventDefault()
+        const len = e.target.length - 1
+        console.log(DEFAULT_OPTIONS[selectedOptionIndex].name)
+        const data = {}
+        data['name'] = DEFAULT_OPTIONS[selectedOptionIndex].name
+        for (let i = 0; i < len; i++) {
+            console.log(e.target[i].id, e.target[i].value)
+            data[e.target[i].id] = e.target[i].value
+        }
+
+        console.log(data)
+        console.log(JSON.stringify(data))
+
+        fetch(`<enter api endpoint here>`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
         })
-    }
 
-    function getImageStyle() {
-        const filters = options.map(option => {
-            return `${option.property}(${option.value}${option.unit})`
-        })
-
-        return { filter: filters.join(' ') }
     }
 
     return (
-        <div className="container2">
-            <div className="container1" id="stp2">
-                <div className="main-image" style={getImageStyle()}>
-                    <img src={props.base64} alt="Image" height="300" width="auto" />
-                    {/* <img src={props.picc} alt="Image" height="300" width="auto" /> */}
 
-                </div>
-                <div className="sidebar">
-                    {options.map((option, index) => {
-                        return (
-                            <SidebarItem
-                                key={index}
-                                name={option.name}
-                                active={index === selectedOptionIndex}
-                                handleClick={() => setSelectedOptionIndex(index)}
-                            />
-                        )
-                    })}
-                </div>
+        <div className="container1">
+            <div className="sidebar">
+                {DEFAULT_OPTIONS.map((augmentation, index) => {
+                    return (
+                        <SidebarItem
+                            key={index}
+                            name={augmentation.name}
+                            active={index === selectedOptionIndex}
+                            handleClick={() => setSelectedOptionIndex(index)}
+                        />
+                    )
+                })}
             </div>
-            <Slider
-                min={selectedOption.range.min}
-                max={selectedOption.range.max}
-                value={selectedOption.value}
-                handleChange={handleSliderChange}
-            />
+            <form onSubmit={(e) => handleSubmit(e)} className="user_inputs">
+                <div className="inputs">
+                    {
+                        DEFAULT_OPTIONS.map((augmentation, index) => {
+                            if (index === selectedOptionIndex) {
+                                return augmentation.parameters.map((parameter) => {
+                                    return (
+                                        <div className="ind_input">
+                                            <label htmlFor={parameter.parameter_name}> Enter {parameter.parameter_name} </label>
+                                            <input
+                                                type={parameter.input_type}
+                                                id={parameter.parameter_name}
+                                                name={parameter.parameter_name}
+                                                // value={parameter.parameter_name}
+                                                // onChange={(e) => `set${parameter.parameter_name}(e.target.value)`}
+                                                step="any"
+                                            />
+                                        </div>
+                                    )
+                                })
+                            }
+                            else return <div></div>
+                        })
+                    }
+                </div>
+                <div className="submit">
+                    <button type="submit">Apply</button>
+                </div>
+            </form>
         </div>
-
     )
 }
 
