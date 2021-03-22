174d5f722e397d26f2f79947cc0945af7814e5b5
fluorspar20 <amoghdixit104@gmail.com>
2021-03-22 23:03:24 +0530

    Step2 done

--- tsr-frontend/src/components/Step2/Step2.js
+++ tsr-frontend/src/components/Step2/Step2.js
@@ -49,31 +49,63 @@ const Step2 = (props) => {
     }
 
     return (
-        <div className="container2">
-            <div className="response-img">
-                <img height="300" src={displayImage} alt="" />
-            </div>
-            <div className="container1">
-                <div className="sidebar">
-                    {DEFAULT_OPTIONS.map((augmentation, index) => {
-                        return (
-                            <SidebarItem
-                                key={index}
-                                name={augmentation.name}
-                                active={index === selectedOptionIndex}
-                                handleClick={() => setSelectedOptionIndex(index)}
-                            />
-                        )
-                    })}
+        <>
+            <div className="container2">
+                <div className="response-img">
+                    <img height="300" src={displayImage} alt="" />
                 </div>
-                <form onSubmit={(e) => handleSubmit(e)} className="user_inputs">
-                    <div className="inputs">
-                        {
-                            DEFAULT_OPTIONS.map((augmentation, index) => {
-                                if (index === selectedOptionIndex) {
-                                    return augmentation.parameters.map((parameter) => {
-                                        if (parameter.parameter_name === 'prob') {
-                                            return (
+            </div>
+            <div>
+                <div className="container1">
+                    <div className="sidebar">
+                        {DEFAULT_OPTIONS.map((augmentation, index) => {
+                            return (
+                                <SidebarItem
+                                    key={index}
+                                    name={augmentation.name}
+                                    active={index === selectedOptionIndex}
+                                    handleClick={() => setSelectedOptionIndex(index)}
+                                />
+                            )
+                        })}
+                    </div>
+                    <form onSubmit={(e) => handleSubmit(e)} className="user_inputs">
+                        <div className="inputs">
+                            {
+                                DEFAULT_OPTIONS.map((augmentation, index) => {
+                                    if (index === selectedOptionIndex) {
+                                        return augmentation.parameters.map((parameter) => {
+                                            if (parameter.parameter_name === 'prob') {
+                                                return (
+                                                    <div className="ind_input">
+                                                        <label htmlFor={parameter.parameter_name}> Enter {parameter.parameter_name} </label>
+                                                        <input
+                                                            type={parameter.input_type}
+                                                            id={parameter.parameter_name}
+                                                            name={parameter.parameter_name}
+                                                            defaultValue={parameter.default_value}
+                                                            min="0"
+                                                            max="1"
+                                                            step="any"
+                                                        />
+                                                    </div>
+                                                )
+                                            }
+                                            if (augmentation.name === 'add_rain') {
+                                                return (
+                                                    <div className="ind_input">
+                                                        <label htmlFor={parameter.parameter_name}> Enter {parameter.parameter_name} (heavy/drizzle) </label>
+                                                        <input
+                                                            type={parameter.input_type}
+                                                            id={parameter.parameter_name}
+                                                            name={parameter.parameter_name}
+                                                            defaultValue={parameter.default_value}
+                                                            step="any"
+                                                        />
+                                                    </div>
+                                                )
+                                            }
+                                            else return (
                                                 <div className="ind_input">
                                                     <label htmlFor={parameter.parameter_name}> Enter {parameter.parameter_name} </label>
                                                     <input
@@ -81,51 +113,23 @@ const Step2 = (props) => {
                                                         id={parameter.parameter_name}
                                                         name={parameter.parameter_name}
                                                         defaultValue={parameter.default_value}
-                                                        min="0"
-                                                        max="1"
                                                         step="any"
                                                     />
                                                 </div>
                                             )
-                                        }
-                                        if (augmentation.name === 'add_rain') {
-                                            return (
-                                                <div className="ind_input">
-                                                    <label htmlFor={parameter.parameter_name}> Enter {parameter.parameter_name} (heavy/drizzle) </label>
-                                                    <input
-                                                        type={parameter.input_type}
-                                                        id={parameter.parameter_name}
-                                                        name={parameter.parameter_name}
-                                                        defaultValue={parameter.default_value}
-                                                        step="any"
-                                                    />
-                                                </div>
-                                            )
-                                        }
-                                        else return (
-                                            <div className="ind_input">
-                                                <label htmlFor={parameter.parameter_name}> Enter {parameter.parameter_name} </label>
-                                                <input
-                                                    type={parameter.input_type}
-                                                    id={parameter.parameter_name}
-                                                    name={parameter.parameter_name}
-                                                    defaultValue={parameter.default_value}
-                                                    step="any"
-                                                />
-                                            </div>
-                                        )
-                                    })
-                                }
-                                else return <div></div>
-                            })
-                        }
-                    </div>
-                    <div className="submit">
-                        <button type="submit">Apply</button>
-                    </div>
-                </form>
+                                        })
+                                    }
+                                    else return <div></div>
+                                })
+                            }
+                        </div>
+                        <div className="submit">
+                            <button type="submit">Apply</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
