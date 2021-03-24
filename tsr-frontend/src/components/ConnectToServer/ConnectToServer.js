908980a2379d843c26e385b7aaffd0bb77772816
fluorspar20 <amoghdixit104@gmail.com>
2021-03-24 16:26:05 +0530

    refactored code

--- tsr-frontend/src/components/ConnectToServer/ConnectToServer.js
+++ tsr-frontend/src/components/ConnectToServer/ConnectToServer.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import './Connect.css'
 
@@ -6,8 +7,22 @@ function ConnectToServer(props) {
     function handleSubmit(e) {
         e.preventDefault()
         console.log(e.target[0].value)
-        props.setUrl(e.target[0].value)
-        alert('The URL has been saved!')
+
+        axios.get(e.target[0].value)
+            .then(res => {
+                console.log(res.status)
+                if (res.status === 200) {
+                    props.setUrl(e.target[0].value)
+                    alert('The URL has been saved!')
+                }
+                else
+                    alert('The URL could not be saved. Please try again.')
+            })
+            .catch(err => {
+                console.log(err)
+                alert('The URL could not be saved. Please try again.')
+            })
+
     }
 
     return (
