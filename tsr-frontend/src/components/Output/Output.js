04df9fbea0eadc4b194b54a3e878b32bceced09e
fluorspar20 <amoghdixit104@gmail.com>
2021-03-27 01:19:31 +0530

    another commit

--- tsr-frontend/src/components/Output/Output.js
+++ tsr-frontend/src/components/Output/Output.js
@@ -3,29 +3,29 @@ import axios from 'axios';
 import * as ReactBootStrap from 'react-bootstrap';
 import Images from "../Images";
 import { makeStyles, Grid } from '@material-ui/core';
-import {Card,CardActionArea,CardActions,CardContent,CardMedia ,Button,Typography } from '@material-ui/core';
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
-      minWidth: 275,
-      elevation: 15,
-      raised: true,
-      marginBottom: "5%",
+        minWidth: 275,
+        elevation: 15,
+        raised: true,
+        marginBottom: "5%",
     },
     bullet: {
-      display: 'inline-block',
-    //   margin: '0 2px',
-      transform: 'scale(0.8)',
+        display: 'inline-block',
+        //   margin: '0 2px',
+        transform: 'scale(0.8)',
     },
     title: {
-      fontSize: 18,
-      color: "#4d4d4d",
-      padding: '0% 3%',
-      paddingBottom: '3%',
-      textAlign:"center",
+        fontSize: 18,
+        color: "#4d4d4d",
+        padding: '0% 3%',
+        paddingBottom: '3%',
+        textAlign: "center",
     },
     pos: {
-    //   marginBottom: 12,
+        //   marginBottom: 12,
     },
     Card: {
         // width: 300,
@@ -35,7 +35,7 @@ const useStyles = makeStyles({
         border: "10px",
         raised: true,
 
-      },
+    },
     //   Media: {
     //     // height: '500px',
     //     height: "100%",
@@ -43,17 +43,17 @@ const useStyles = makeStyles({
     //     // width: '100px',
     //     objectFit: 'cover'
     //   },
-  });
+});
 
 export default function Output(props) {
 
     const classes = useStyles();
-    console.log(props.url , "thiss")
+    console.log(props.url, "thiss")
 
     return (
-        <div className="dashboard" style={{marginLeft: "240px", paddingBottom: "10%", backgroundColor: 'rgb(211 211 211 / 15%)' ,}}  alignItems="center" direction="column"
-        alignItems="center">
-            <header style={{textAlign: "center", fontWeight: '900',}}>
+        <div className="dashboard" style={{ marginLeft: "240px", paddingBottom: "10%", backgroundColor: 'rgb(211 211 211 / 15%)', }} alignItems="center" direction="column"
+            alignItems="center">
+            <header style={{ textAlign: "center", fontWeight: '900', }}>
                 <h1>Baseline Model</h1>
             </header>
             <div className={classes.root}>
@@ -61,72 +61,72 @@ export default function Output(props) {
             <Grid container>
                 <Grid item md={1}></Grid>
                 <Grid item md={10}>
-                <Card className={classes.root} variant="outlined" style={{margin: "10px"}} elevation={15}>
-                <CardContent alignItems="center"  className={classes.Media}>
-                    <h5 style={{textAlign: "center", fontWeight: '900',}}>Number of images per class</h5>
-                    <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    </Typography>
-                    {/* <img src="/assets/training_images_per_category_sorted.png" height="550px" alt="" /> */}
-                    <Images url={props.url+'/displayImages?id=image_data_distribution.png'} height={"550px"}/>
-                    
-                </CardContent>
-                 
-                </Card>  
+                    <Card className={classes.root} variant="outlined" style={{ margin: "10px" }} elevation={15}>
+                        <CardContent alignItems="center" className={classes.Media}>
+                            <h5 style={{ textAlign: "center", fontWeight: '900', }}>Number of images per class</h5>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            </Typography>
+                            {/* <img src="/assets/training_images_per_category_sorted.png" height="550px" alt="" /> */}
+                            <Images url={props.url + '/displayImages?id=image_data_distribution.png'} height={"550px"} />
+
+                        </CardContent>
+
+                    </Card>
                 </Grid>
                 <Grid item md={1}></Grid>
                 <Grid item md={1}></Grid>
                 <Grid item md={5}>
-                    <Card className={classes.root} variant="outlined" style={{margin: "10px"}}>
-                    <CardContent alignItems="center" style={{}} className={classes.Media}>
-                    <h5 style={{textAlign: "center", fontWeight: '900',}} >Training and Validation Accuracy</h5>
-                    <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    </Typography>
-                        {/* <img src="/assets/TrainingandValidationAccuracy.jpeg"    height="350px"  alt="" /> */}
-                        <Images url={props.url+'/displayImages?id=accuracy.png'} height={"350px"} />
-                    </CardContent>
-                     
+                    <Card className={classes.root} variant="outlined" style={{ margin: "10px" }}>
+                        <CardContent alignItems="center" style={{}} className={classes.Media}>
+                            <h5 style={{ textAlign: "center", fontWeight: '900', }} >Training and Validation Accuracy</h5>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            </Typography>
+                            {/* <img src="/assets/TrainingandValidationAccuracy.jpeg"    height="350px"  alt="" /> */}
+                            <Images url={props.url + '/displayImages?id=accuracy.png'} height={"350px"} />
+                        </CardContent>
+
                     </Card>
                 </Grid>
                 <Grid item md={5}>
-                    <Card className={classes.root} variant="outlined" style={{margin: "10px"}}>
-                    <CardContent alignItems="center" style={{}} className={classes.Media}>
-                    <h5 style={{textAlign: "center", fontWeight: '900',}}>Training and Validation Loss</h5>
-                    <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    </Typography>
-                        {/* <img src="/assets/TrainingandValidationLoss.jpeg" height="350px" alt="" /> */}
-                        <Images url={props.url+'/displayImages?id=loss.png'} height={"350px"} />
-                        
-                    </CardContent>
-                     
+                    <Card className={classes.root} variant="outlined" style={{ margin: "10px" }}>
+                        <CardContent alignItems="center" style={{}} className={classes.Media}>
+                            <h5 style={{ textAlign: "center", fontWeight: '900', }}>Training and Validation Loss</h5>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            </Typography>
+                            {/* <img src="/assets/TrainingandValidationLoss.jpeg" height="350px" alt="" /> */}
+                            <Images url={props.url + '/displayImages?id=loss.png'} height={"350px"} />
+
+                        </CardContent>
+
                     </Card>
                 </Grid>
                 <Grid item md={1}></Grid>
 
                 <Grid item md={1}></Grid>
                 <Grid item md={10}>
-                    <Card className={classes.root} variant="outlined" style={{margin: "10px"}}>
-                    <CardContent alignItems="center" style={{}} className={classes.Media}>
-                    <h5 style={{textAlign: "center", fontWeight: '900',}}>Prediction of the Classes of Images</h5>
-                    {/* style={{width:'fit-content', padding:"10px"}} */}
-                    {/* <Typography className={classes.title} color="textSecondary" gutterBottom> </Typography> */}
-                        {/* <img src="/assets/PredictionoftheClassesofImages.jpeg" height="1000"  alt=""  /> */}
-                        <Images url={props.url+'/displayImages?id=bar_chart.png'} height={"900"} />   
-                    </CardContent>
-                     
+                    <Card className={classes.root} variant="outlined" style={{ margin: "10px" }}>
+                        <CardContent alignItems="center" style={{}} className={classes.Media}>
+                            <h5 style={{ textAlign: "center", fontWeight: '900', }}>Prediction of the Classes of Images</h5>
+                            {/* style={{width:'fit-content', padding:"10px"}} */}
+                            {/* <Typography className={classes.title} color="textSecondary" gutterBottom> </Typography> */}
+                            {/* <img src="/assets/PredictionoftheClassesofImages.jpeg" height="1000"  alt=""  /> */}
+                            <Images url={props.url + '/displayImages?id=bar_chart.png'} height={"900"} />
+                        </CardContent>
+
                     </Card>
                 </Grid>
                 <Grid item md={1}></Grid>
                 <Grid item md={1}></Grid>
                 <Grid item md={10}>
-                    <Card className={classes.root} variant="outlined" style={{margin: "10px"}}>
-                    <CardContent alignItems="center" style={{}} className={classes.Media}>
-                    <h5 style={{textAlign: "center", fontWeight: '900',}}>Classwise Accuracy Heatmap</h5>
-                        <Typography className={classes.title} color="textSecondary" gutterBottom>
-                        </Typography>
-                        {/* <img src="/assets/ClasswiseAccuracyHeatmap.jpeg" height="1000" alt=""  /> */}
-                        <Images url={props.url+'/displayImages?id=confusion_matrix.png'} height={"1000"} />  
-                    </CardContent>
-                     
+                    <Card className={classes.root} variant="outlined" style={{ margin: "10px" }}>
+                        <CardContent alignItems="center" style={{}} className={classes.Media}>
+                            <h5 style={{ textAlign: "center", fontWeight: '900', }}>Classwise Accuracy Heatmap</h5>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            </Typography>
+                            {/* <img src="/assets/ClasswiseAccuracyHeatmap.jpeg" height="1000" alt=""  /> */}
+                            <Images url={props.url + '/displayImages?id=confusion_matrix.png'} height={"1000"} />
+                        </CardContent>
+
                     </Card>
                 </Grid>
                 <Grid item md={1}></Grid>
@@ -141,9 +141,9 @@ export default function Output(props) {
                             <img src="/assets/_roc_curve.png" height="600" style={{width: '100%'}} alt="" />
                             </div> */}
                             <Typography className={classes.title} color="textSecondary" gutterBottom> </Typography>
-                            <Images url={props.url+'/displayImages?id=_roc_curve.png'} height={"600"} />
+                            <Images url={props.url + '/displayImages?id=_roc_curve.png'} height={"600"} />
                         </CardContent>
-                         
+
                     </Card>
                 </Grid>
                 <Grid item md={1}></Grid>
@@ -151,15 +151,15 @@ export default function Output(props) {
                 <Grid item md={10}>
                     <Card className={classes.root} variant="outlined" style={{ margin: "10px" }}>
                         <CardContent alignItems="center" style={{}} className={classes.Media}>
-                            <h5 style={{ textAlign: "center", fontWeight: '900', }}>visualising The Activations Of Different Layers</h5>
+                            <h5 style={{ textAlign: "center", fontWeight: '900', }}>Visualising The Activations Of Different Layers</h5>
                             {/* style={{width:'fit-content', padding:"10px"}} */}
                             {/* <div style={{width: "fit-content"}}>
                             <img src="/assets/visualisingTheActivationsOfDifferentLayers.jpeg" height="600" style={{width: '100%'}} alt="" />
                             </div> */}
                             <Typography className={classes.title} color="textSecondary" gutterBottom> </Typography>
-                            <Images url={props.url+'/displayImages?id=AL0.png'} height={"600"} />
+                            <Images url={props.url + '/displayImages?id=AL0.png'} height={"600"} />
                         </CardContent>
-                         
+
                     </Card>
                 </Grid>
                 <Grid item md={1}></Grid>
@@ -173,9 +173,9 @@ export default function Output(props) {
                             <img src="/assets/os.png" height="200" style={{width: '100%', margin: '0 auto'}} alt="" />
                             </div> */}
                             <Typography className={classes.title} color="textSecondary" gutterBottom> </Typography>
-                            <Images url={props.url+'/displayImages?id=os.png'} height={"200"} />
+                            <Images url={props.url + '/displayImages?id=os.png'} height={"200"} />
                         </CardContent>
-                         
+
                     </Card>
                 </Grid>
                 <Grid item md={1}></Grid>
@@ -189,9 +189,9 @@ export default function Output(props) {
                             {/* <img src="/assets/ClassificationReport.jpeg" height="900" style={{width: '100%', margin: '0 auto !important'}} alt="" /> */}
                             {/* </div> */}
                             <Typography className={classes.title} color="textSecondary" gutterBottom> </Typography>
-                            <Images url={props.url+'/displayImages?id=pil_text.png'} height={"900"} />
+                            <Images url={props.url + '/displayImages?id=pil_text.png'} height={"900"} />
                         </CardContent>
-                         
+
                     </Card>
                 </Grid>
                 <Grid item md={1}></Grid>
